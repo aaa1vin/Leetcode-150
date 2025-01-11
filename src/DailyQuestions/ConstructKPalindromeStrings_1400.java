98169914e0d5f938305d8a6abf6c409b9b3af599
Alvin <alvinong96@hotmail.com>
2025-01-11 21:19:46 +0800

    refine 11 Jan

--- src/DailyQuestions/ConstructKPalindromeStrings_1400.java
+++ src/DailyQuestions/ConstructKPalindromeStrings_1400.java
@@ -9,22 +9,17 @@ public class ConstructKPalindromeStrings_1400 {
         High level logic: add long as there are odd number counts of char greater than k,
         it will not be able to meet the permutations k
          */
+        if (s.length() < k) System.out.println(false);
         int[] cnt = new int[26];
 
         for (char c : s.toCharArray()) {
             cnt[c - 'a']++;
         }
 
-        if (s.length() < k) System.out.println(false);
-        if (s.length() == k) System.out.println(true);
-
         int odd = 0;
         for (int x: cnt) {
-            if (x %2 == 1) {
-                odd++;
-            }
+            if (x %2 == 1) odd++;
         }
-        if (odd > k) System.out.println(false);
         System.out.println(String.valueOf(odd<= k));
     }
 }
